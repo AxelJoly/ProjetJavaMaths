ff2c65c231adec191286e07e87bcf4b63a9d4b2b
Joly Axel <joly.axel@outlook.fr>
2016-12-05 16:10:29 +0100

    La musiqueeeeee

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,30 @@
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javazoom.jl.player.*;
+import java.io.FileInputStream;
+
 
 /**
  * Created by axel on 05/12/2016.
  */
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-
-    public static void main (String args[]){
+    public Main(){
         LOGGER.info("mdr ca marche");
-        LOGGER.info("vibouille est un enfant");
-        LOGGER.info("Test");
-        LOGGER.info("vibouille est un enfant ");
-        LOGGER.info("vibouille est un enfant ");
-        LOGGER.info("vibouille est un enfant ");
+        try{
 
+            FileInputStream fis = new FileInputStream("bip.mp3");
+            Player playMP3 = new Player(fis);
 
+            playMP3.play();
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+    public static void main (String args[]){
+        new Main();
     }
 }
