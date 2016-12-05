7f7120b4b0cd32f9f2e5e06076e1469a09f01e74
Joly Axel <joly.axel@outlook.fr>
2016-12-05 12:47:18 +0100

    Premier commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by axel on 05/12/2016.
+ */
+public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main (String args[]){
+        LOGGER.info("mdr ca marche");
+        LOGGER.info("vibouille est un enfant");
+        LOGGER.info("Test");
+        LOGGER.info("vibouille est un enfant ");
+
+    }
+}
