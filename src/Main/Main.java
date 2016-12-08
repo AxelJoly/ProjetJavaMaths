9d8673f50e8be12313095a4017b1113203e4605d
Joly Axel <joly.axel@outlook.fr>
2016-12-08 09:06:03 +0100

    Graphique + FFT

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,11 @@
 
 package Main;
 
+import Controller.FrameController;
 import Model.Complexe;
 import Model.MusicThread;
 import Model.Nombre;
-
+import Model.Echantillonage;
 
 import View.Frame;
 
@@ -31,6 +32,20 @@ public class Main {
 
         Complexe[] tab = {new Complexe(1, 0), new Complexe(1, 0), new Complexe(1, 0), new Complexe(1, 0)};
         Complexe[] result;
+        Complexe[] tab2 = new Complexe[1000];
+        for(int i=0;i<16;i++) {
+            tab2[i] = new Complexe(i, 0);
+        }
+        Echantillonage ech=new Echantillonage(4,16,tab2);
+        ech.rempliTab0();
+        ech.calculColonneAj();
+        for(int j=0;j<(ech.getNbPoints()/ech.getEchantillonage()); j++) {
+            for (int i = 0; i < ech.getEchantillonage(); i++) {
+                LOGGER.info(ech.getMatrice()[j][i].toString());
+            }
+        }
+        ech.selectioneAj();
+
         Nombre re = new Nombre(2);
        result = re.calculFourierRapide(tab);
 
@@ -39,11 +54,12 @@ public class Main {
         for (int i = 0; i < length; i++){
             LOGGER.info(result[i].toString());
         }
-
+        FrameController controller = new FrameController(re);
         Thread music = new Thread(new MusicThread());
        // music.start();
 
-        Frame frame = new Frame();
+        Frame frame = new Frame(re, controller);
+        controller.init(frame);
         frame.display();
 
     }
